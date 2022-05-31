b250223fabb04030fdb50887e078556666526af6
henningreck <reck.henning2@gmail.com>
2022-05-31 10:35:34 +0200

    L09: Done

--- Aufgaben/L09/js/script.js
+++ Aufgaben/L09/js/script.js
@@ -1,48 +1,30 @@
-function playSample(file) {
-    var sound = new Audio("assets/keyboard/" + file);
-    sound.play();
-}
-document.querySelector("#c.whitekey").addEventListener("click", function () {
-    playSample("c.mp3");
-});
-document.querySelector("#d.whitekey").addEventListener("click", function () {
-    playSample("d.mp3");
-});
-document.querySelector("#e.whitekey").addEventListener("click", function () {
-    playSample("e.mp3");
-});
-document.querySelector("#f.whitekey").addEventListener("click", function () {
-    playSample("f.mp3");
-});
-document.querySelector("#g.whitekey").addEventListener("click", function () {
-    playSample("g.mp3");
-});
-document.querySelector("#a.whitekey").addEventListener("click", function () {
-    playSample("a.mp3");
-});
-document.querySelector("#b.whitekey").addEventListener("click", function () {
-    playSample("b.mp3");
-});
-document.querySelector("#cf.blackkey_two").addEventListener("click", function () {
-    playSample("df.mp3");
-});
-document.querySelector("#df.blackkey_two").addEventListener("click", function () {
-    playSample("ef.mp3");
-});
-document.querySelector("#ff.blackkey_three").addEventListener("click", function () {
-    playSample("gf.mp3");
-});
-document.querySelector("#gf.blackkey_three").addEventListener("click", function () {
-    playSample("af.mp3");
-});
-document.querySelector("#af.blackkey_three").addEventListener("click", function () {
-    playSample("bf.mp3");
-});
+// function playSample(file: string): void {
+//     var sound: HTMLAudioElement = new Audio("assets/keyboard/" + file);
+//     sound.play();
+// }
 var sounds = ["a.mp3", "b.mp3", "a.mp3", "b.mp3", "e.mp3"];
+var allsounds = ["a.mp3", "b.mp3", "c.mp3", "d.mp3", "e.mp3", "f.mp3", "g.mp3", "af.mp3", "bf.mp3", "df.mp3", "ef.mp3", "gf.mp3"];
 var active = 0;
 var playbutton = document.querySelector("#playbutton");
 let interval;
 let intervalstatus = false;
+let index;
+document.querySelector("#c.whitekey").addEventListener("click", function () { playSample("c.mp3"); });
+document.querySelector("#d.whitekey").addEventListener("click", function () { playSample("d.mp3"); });
+document.querySelector("#e.whitekey").addEventListener("click", function () { playSample("e.mp3"); });
+document.querySelector("#f.whitekey").addEventListener("click", function () { playSample("f.mp3"); });
+document.querySelector("#g.whitekey").addEventListener("click", function () { playSample("g.mp3"); });
+document.querySelector("#a.whitekey").addEventListener("click", function () { playSample("a.mp3"); });
+document.querySelector("#b.whitekey").addEventListener("click", function () { playSample("b.mp3"); });
+document.querySelector("#df.blackkey_two").addEventListener("click", function () { playSample("df.mp3"); });
+document.querySelector("#ef.blackkey_two").addEventListener("click", function () { playSample("ef.mp3"); });
+document.querySelector("#gf.blackkey_three").addEventListener("click", function () { playSample("gf.mp3"); });
+document.querySelector("#af.blackkey_three").addEventListener("click", function () { playSample("af.mp3"); });
+document.querySelector("#bf.blackkey_three").addEventListener("click", function () { playSample("bf.mp3"); });
+function playSample(file) {
+    var sound = new Audio("assets/keyboard/" + file);
+    sound.play();
+}
 function Interval() {
     if (intervalstatus == false) {
         intervalstatus = true;
@@ -58,8 +40,8 @@ function Interval() {
 }
 function playInterval() {
     if (playbutton.className === "go") {
-        var sound = new Audio("assets/keyboard/" + sounds[active]);
-        sound.play();
+        // var sound: HTMLAudioElement = new Audio("assets/keyboard/" + sounds[active]);
+        playSample(sounds[active]);
         active++;
         if (active == 5) {
             active = 0;
@@ -76,11 +58,11 @@ document.querySelector("#playbutton").addEventListener("click", function () {
 });
 document.querySelector("#play").addEventListener("click", Interval);
 // REMIX
-var remixsounds = ["a.mp3", "b.mp3", "c.mp3", "d.mp3", "e.mp3", "f.mp3", "g.mp3", "af.mp3", "bf.mp3", "df.mp3", "ef.mp3", "gf.mp3"];
 var btncontainer = document.querySelector("#remix_container");
 let remixinterval;
 var remixactive;
 let remixintervalstatus = false;
+let beats = [0, 1, 2, 1];
 function RemixInterval() {
     if (remixintervalstatus == false) {
         remixintervalstatus = true;
@@ -91,17 +73,15 @@ function RemixInterval() {
         if (remixactive != 0) {
             remixactive = 0;
         }
+        clearInterval(remixinterval);
     }
 }
 function playRemixInterval() {
-    var remixsound = new Audio("assets/keyboard/" + remixsounds[remixactive]);
-    remixsound.play();
-    remixactive = (setInterval(function () {
-        Math.floor(Math.random() * (11 - 1 + 1) + 1);
-    }, 500));
+    remixactive = randomfunction(0, 11);
+    playSample(allsounds[remixactive]);
 }
-function randomnumber(min, max) {
+function randomfunction(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-document.querySelector("#btn").addEventListener("click", playRemixInterval);
+document.querySelector("#btn").addEventListener("click", RemixInterval);
 //# sourceMappingURL=script.js.map
--- Aufgaben/L09/js/script.ts
+++ Aufgaben/L09/js/script.ts
@@ -1,53 +1,36 @@
-function playSample(file: string): void {
-    var sound: HTMLAudioElement = new Audio("assets/keyboard/" + file);
-    sound.play();
-}
-
-document.querySelector("#c.whitekey").addEventListener("click", function(): void {
-    playSample("c.mp3");
-});
-document.querySelector("#d.whitekey").addEventListener("click", function(): void {
-    playSample("d.mp3");
-});
-document.querySelector("#e.whitekey").addEventListener("click", function(): void {
-    playSample("e.mp3");
-});
-document.querySelector("#f.whitekey").addEventListener("click", function(): void {
-    playSample("f.mp3");
-});
-document.querySelector("#g.whitekey").addEventListener("click", function(): void {
-    playSample("g.mp3");
-});
-document.querySelector("#a.whitekey").addEventListener("click", function(): void {
-    playSample("a.mp3");
-});
-document.querySelector("#b.whitekey").addEventListener("click", function(): void {
-    playSample("b.mp3");
-});
-
-document.querySelector("#cf.blackkey_two").addEventListener("click", function(): void {
-    playSample("df.mp3");
-});
-document.querySelector("#df.blackkey_two").addEventListener("click", function(): void {
-    playSample("ef.mp3");
-});
-document.querySelector("#ff.blackkey_three").addEventListener("click", function(): void {
-    playSample("gf.mp3");
-});
-document.querySelector("#gf.blackkey_three").addEventListener("click", function(): void {
-    playSample("af.mp3");
-});
-document.querySelector("#af.blackkey_three").addEventListener("click", function(): void {
-    playSample("bf.mp3");
-});
-
+// function playSample(file: string): void {
+//     var sound: HTMLAudioElement = new Audio("assets/keyboard/" + file);
+//     sound.play();
+// }
 
 var sounds: string [] = ["a.mp3", "b.mp3", "a.mp3", "b.mp3", "e.mp3"];
+var allsounds: string [] = ["a.mp3", "b.mp3", "c.mp3", "d.mp3", "e.mp3", "f.mp3", "g.mp3", "af.mp3", "bf.mp3", "df.mp3", "ef.mp3", "gf.mp3"];
 var active: number = 0;
 var playbutton: HTMLDivElement = document.querySelector("#playbutton");
 let interval: number;
 let intervalstatus: boolean = false;
+let index: number; 
 
+document.querySelector("#c.whitekey").addEventListener("click", function(): void {playSample("c.mp3"); });
+document.querySelector("#d.whitekey").addEventListener("click", function(): void {playSample("d.mp3"); });
+document.querySelector("#e.whitekey").addEventListener("click", function(): void {playSample("e.mp3"); });
+document.querySelector("#f.whitekey").addEventListener("click", function(): void {playSample("f.mp3"); });
+document.querySelector("#g.whitekey").addEventListener("click", function(): void {playSample("g.mp3"); });
+document.querySelector("#a.whitekey").addEventListener("click", function(): void {playSample("a.mp3"); });
+document.querySelector("#b.whitekey").addEventListener("click", function(): void {playSample("b.mp3"); });
+
+document.querySelector("#df.blackkey_two").addEventListener("click", function(): void {playSample("df.mp3"); });
+document.querySelector("#ef.blackkey_two").addEventListener("click", function(): void {playSample("ef.mp3"); });
+document.querySelector("#gf.blackkey_three").addEventListener("click", function(): void {playSample("gf.mp3"); });
+document.querySelector("#af.blackkey_three").addEventListener("click", function(): void {playSample("af.mp3"); });
+document.querySelector("#bf.blackkey_three").addEventListener("click", function(): void {playSample("bf.mp3"); });
+
+
+
+function playSample(file: string): void {
+    var sound: HTMLAudioElement = new Audio("assets/keyboard/" + file);
+    sound.play();
+}
 
 function Interval(): void {
     if (intervalstatus == false) {
@@ -64,8 +47,8 @@ function Interval(): void {
 
 function playInterval(): void {
     if (playbutton.className === "go") {
-        var sound: HTMLAudioElement = new Audio("assets/keyboard/" + sounds[active]);
-        sound.play();
+        // var sound: HTMLAudioElement = new Audio("assets/keyboard/" + sounds[active]);
+        playSample(sounds[active]);
         active++;
         if (active == 5) {
             active = 0;
@@ -88,12 +71,11 @@ document.querySelector("#play").addEventListener("click", Interval);
 
 // REMIX
 
-var remixsounds: string [] = ["a.mp3", "b.mp3", "c.mp3", "d.mp3", "e.mp3", "f.mp3", "g.mp3", "af.mp3", "bf.mp3", "df.mp3", "ef.mp3", "gf.mp3"];
-
 var btncontainer: HTMLDivElement = document.querySelector("#remix_container");
 let remixinterval: number;
 var remixactive: number;
 let remixintervalstatus: boolean = false;
+let beats: number[] = [0, 1, 2, 1];
 
 function RemixInterval(): void {
 
@@ -105,22 +87,18 @@ if (remixintervalstatus == false) {
         if (remixactive != 0) {
             remixactive = 0; 
         }
+        clearInterval(remixinterval);
     }
 }
     
 function playRemixInterval(): void {
-    var remixsound: HTMLAudioElement = new Audio("assets/keyboard/" + remixsounds[remixactive]);
-    remixsound.play();
-    remixactive = (setInterval(function(): void {
-        Math.floor(Math.random() * (11 - 1 + 1) + 1);
-    },                         500));
-
+    remixactive = randomfunction(0, 11);
+    playSample(allsounds[remixactive]);
     }
 
 
-function randomnumber(min: number, max: number) {
+function randomfunction (min: number, max: number): number {   
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
-document.querySelector("#btn").addEventListener("click", playRemixInterval);
+document.querySelector("#btn").addEventListener("click", RemixInterval);
