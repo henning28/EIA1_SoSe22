32d0dd3016c1a05bd70047e8a317a394124b56eb
henningreck <reck.henning2@gmail.com>
2022-06-04 22:26:26 +0200

    WIP: L10

--- Aufgaben/L10/js/script.js
+++ Aufgaben/L10/js/script.js
@@ -1,30 +1,69 @@
 let addbutton = document.getElementById("#addbutton");
 let count = 1;
+let i = 1;
 let taskdiv = document.getElementById("tasks");
-document.querySelector("#addnewtask").addEventListener("click", addTask);
+let yes = document.querySelector(".task_container");
+// EventListeners
+document.querySelector("#addnewtask").addEventListener("click", function () {
+    addTask();
+    clearInput();
+});
+document.querySelector(".fa-trash-can").addEventListener("click", deleteTask);
+// Functions
+function deleteTask() {
+    yes.remove();
+}
 function addTask() {
-    let i = 7;
+    let userInput = document.querySelector("input").value;
+    i++;
     var creatediv = document.createElement("div");
     creatediv.className = "task_container" + i;
     taskdiv.appendChild(creatediv);
+    creatediv.classList.add("task_containerstyle");
     var createfirstdiv = document.createElement("div");
     createfirstdiv.className = "task_one" + i;
+    createfirstdiv.classList.add("task_onestyle");
+    var createseconddiv = document.createElement("div");
+    createseconddiv.className = "task_two" + i;
+    createseconddiv.classList.add("task_twostyle");
+    var createthirddiv = document.createElement("div");
+    createthirddiv.className = "task_three" + i;
+    createthirddiv.classList.add("task_threestyle");
+    var innertaskcontainer = document.querySelector(".task_container" + i);
+    innertaskcontainer.appendChild(createfirstdiv);
+    innertaskcontainer.appendChild(createseconddiv);
+    innertaskcontainer.appendChild(createthirddiv);
     var createcheck = document.createElement("div");
     createcheck.classList.add("fa-regular");
     createcheck.classList.add("fa-circle");
     createcheck.classList.add("fa-2xl");
-    var innertaskcontainer = document.querySelector(".task_container" + i);
-    innertaskcontainer.appendChild(createfirstdiv);
-    var createseconddiv = document.createElement("div");
-    createseconddiv.className = "task_two" + i;
     var innertaskone = document.querySelector(".task_one" + i);
     innertaskone.appendChild(createcheck);
+    var createtext = document.createElement("p");
+    createtext.classList.add("tasktext" + i);
+    createtext.textContent = userInput;
+    var innertasktwo = document.querySelector(".task_two" + i);
+    innertasktwo.appendChild(createtext);
+    var createtrash = document.createElement("div");
+    createtrash.classList.add("fa-regular");
+    createtrash.classList.add("fa-trash-can");
+    createtrash.classList.add("fa-2xl");
+    var innertaskthree = document.querySelector(".task_three" + i);
+    innertaskthree.appendChild(createtrash);
+    document.getElementById("totaltasks").innerText = i + " in total";
 }
-//add Div mit class="task_container"
-//add Div mit class="first_task1"
-//add i mit class="fa-regular fa-circle fa-2xl"
-//add Div mit class="first_task2"
-//add p mit class="task1_text" "input"
-//add Div mit class="first_task3"
-//add i mit class="fa-regular fa-trash-can fa-2xl"
+function clearInput() {
+    document.querySelector("input").value = "";
+}
+// document.querySelector(".task_container").addEventListener("click", function(): void {
+//     changecheck();
+// });
+function changecheck() {
+    if (document.querySelector(".task_onestyle div").getAttribute("class") == "fa-regular fa-circle-check fa-2xl") {
+        document.querySelector(".task_onestyle div").setAttribute("class", "fa-regular fa-circle fa-2xl");
+    }
+    else {
+        document.querySelector(".task_onestyle div").setAttribute("class", "fa-regular fa-circle-check fa-2xl");
+    }
+}
 //# sourceMappingURL=script.js.map
--- Aufgaben/L10/js/script.ts
+++ Aufgaben/L10/js/script.ts
@@ -1,51 +1,94 @@
 
 let addbutton: HTMLElement = document.getElementById("#addbutton");
 let count: number = 1;
+let i: number = 1;
 
 let taskdiv: HTMLElement = document.getElementById("tasks");
 
+let yes: HTMLElement = document.querySelector(".task_container");
 
-document.querySelector("#addnewtask").addEventListener("click", addTask);
+// EventListeners
+
+document.querySelector("#addnewtask").addEventListener("click", function(): void {
+    addTask();
+    clearInput(); 
+});
+
+document.querySelector(".fa-trash-can").addEventListener("click", deleteTask);
+
+// Functions
+
+function deleteTask (): void {
+    yes.remove();
+}
 
 function addTask(): void {
-    let i: number = 7;
+    let userInput: string = document.querySelector("input").value;
+    i++;
+
     var creatediv: HTMLDivElement = document.createElement("div");
     creatediv.className = "task_container" + i;
     taskdiv.appendChild(creatediv);
+    creatediv.classList.add("task_containerstyle");
 
     var createfirstdiv: HTMLDivElement = document.createElement("div");
     createfirstdiv.className = "task_one" + i;
+    createfirstdiv.classList.add("task_onestyle");
+
+    var createseconddiv: HTMLDivElement = document.createElement("div");
+    createseconddiv.className = "task_two" + i;
+    createseconddiv.classList.add("task_twostyle");
+
+    var createthirddiv: HTMLDivElement = document.createElement("div");
+    createthirddiv.className = "task_three" + i;
+    createthirddiv.classList.add("task_threestyle");
+
+    var innertaskcontainer: HTMLDivElement = document.querySelector(".task_container" + i);
+    innertaskcontainer.appendChild(createfirstdiv);
+    innertaskcontainer.appendChild(createseconddiv);
+    innertaskcontainer.appendChild(createthirddiv);
 
     var createcheck: HTMLDivElement = document.createElement("div");
     createcheck.classList.add("fa-regular");
     createcheck.classList.add("fa-circle");
     createcheck.classList.add("fa-2xl");
 
-    var innertaskcontainer: HTMLDivElement = document.querySelector(".task_container" + i);
-    innertaskcontainer.appendChild(createfirstdiv);
-
-    var createseconddiv: HTMLDivElement = document.createElement("div");
-    createseconddiv.className = "task_two" + i;
-
     var innertaskone: HTMLDivElement = document.querySelector(".task_one" + i);
     innertaskone.appendChild(createcheck);
+
+    var createtext: HTMLParagraphElement = document.createElement("p");
+    createtext.classList.add("tasktext" + i);
+    createtext.textContent = userInput;
+
+    var innertasktwo: HTMLDivElement = document.querySelector(".task_two" + i);
+    innertasktwo.appendChild(createtext);
+
+    var createtrash: HTMLDivElement = document.createElement("div");
+    createtrash.classList.add("fa-regular");
+    createtrash.classList.add("fa-trash-can");
+    createtrash.classList.add("fa-2xl");
+
+    var innertaskthree: HTMLDivElement = document.querySelector(".task_three" + i);
+    innertaskthree.appendChild(createtrash);
+
+    document.getElementById("totaltasks").innerText = i + " in total";
+
 }
 
 
+function clearInput (): void {
+    document.querySelector("input").value = "";
+}
 
 
+// document.querySelector(".task_container").addEventListener("click", function(): void {
+//     changecheck();
+// });
 
-
-
-
-
-
-
-
-    //add Div mit class="task_container"
-    //add Div mit class="first_task1"
-    //add i mit class="fa-regular fa-circle fa-2xl"
-    //add Div mit class="first_task2"
-    //add p mit class="task1_text" "input"
-    //add Div mit class="first_task3"
-    //add i mit class="fa-regular fa-trash-can fa-2xl"
+function changecheck (): void {
+    if (document.querySelector(".task_onestyle div").getAttribute("class") == "fa-regular fa-circle-check fa-2xl"
+    ) {
+        document.querySelector(".task_onestyle div").setAttribute("class", "fa-regular fa-circle fa-2xl");
+    } else {
+        document.querySelector(".task_onestyle div").setAttribute("class", "fa-regular fa-circle-check fa-2xl");
+}}
